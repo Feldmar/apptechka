4a67864a90dd9a4fed56ec5131192a05f2b8e1df
Anastasiya Boltach <anastasiya.boltach@brotherhood.software>
2026-07-07 01:04:09 +0300

    перенесен бэкенд

--- src/api/client.ts
+++ src/api/client.ts
@@ -10,7 +10,7 @@ import type {
 } from '../types'
 import { clearToken, getToken, setToken } from '../utils/authStorage'
 
-const API_BASE = '/api'
+const API_BASE = import.meta.env.VITE_API_URL ?? "http://localhost:5000/api";
 
 type UnauthorizedHandler = () => void
 
